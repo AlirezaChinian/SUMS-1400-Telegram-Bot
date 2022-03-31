86bcfe25818968ba56d12898922653336b91cc20
Alireza Chinian <alirezachinian@gmail.com>
2022-04-01 04:02:54 +0430

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,28 @@
-# set tokens that Botfather gave it to you
-# you should intet two different token
+# set tokens that @Botfather gave it to you
+# you should enter two different token
 MAIN_BOT_TOKEN = ""
 SUPPORT_BOT_TOKEN = ""
 
-# site url or server ip flaskapp running on it
+# site url or server ip that flaskapp deployed on it
 SITE_ADDRESS = ""
 
-# security token for urls in flaskapp more than 50 char
+# security token for urls in flaskapp.it's better to be >= 50 char
 SEC_TOKEN = ""
 
 # security key for flaskapp
 SECRET_KEY = ""
 
-# add channel id like @test (it should contain @)
+# add channel id like @test (it should contain @).it just work for public channels
 CHANNEL = ""
 
 # set admins user_id in this list. values should be integer not string
 ADMINS = []
 
-# max numbers of messages user sent that anti flood can accept
+# max numbers of messages user sent that anti flood can accept and mmore than message will enable block mode for user
+# this method is just for common users and it doesn't block admins
 MAX_USAGE = 5
 
-# number of users that want you to create in flaskapp
+# number of users that want you to create in flask
 # 4 is my option change it with your custom one
+# you should set user ids in database
 NUM_USER_CREATE = 4
